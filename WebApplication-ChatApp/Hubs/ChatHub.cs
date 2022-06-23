794151c408a774fca9ffb9f58be25d4cfe65befe
unknown <thushiravirajan01@gmail.com>
2022-06-23 07:26:13 +0530

    Initial Commit

---
+++ WebApplication-ChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_ChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        //ReceiveMessage is a method  same in Angular Application
+        //Define Method
+        public async Task SendMessage (string userName,string Message)
+            {
+            await Clients.All.SendAsync("ReceiveMessage", userName, Message);
+            }
+        
+    }
+}
